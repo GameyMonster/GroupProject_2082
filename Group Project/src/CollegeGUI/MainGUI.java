61f265010acc4744b9db2d6705577ec4d1873585
Joshua Vang <vangjosh95@gmail.com>
2019-11-25 10:02:59 -0600

    Create JTable and JComboBox

--- Group Project/src/CollegeGUI/MainGUI.java
+++ Group Project/src/CollegeGUI/MainGUI.java
@@ -8,16 +8,23 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.border.MatteBorder;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import javax.swing.JSpinner;
+import javax.swing.JComboBox;
+import javax.swing.JScrollBar;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.DefaultComboBoxModel;
 
 public class MainGUI extends JFrame {
-
-	private JPanel contentPane;
 	private JTextField txtCourseName;
-	private JTextField txtCourseID;
+	private JTextField txtCourseId;
 	private JTextField txtInstructor;
-	private JTextField txtCredit;
-	private JTextField txtDays;
-	private JTextField txtTime;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -41,79 +48,120 @@ public class MainGUI extends JFrame {
 	public MainGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 500);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		getContentPane().setLayout(null);
+		getContentPane().setLayout(null);
+		getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(313, 55, 450, 342);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 0, 450, 342);
+		panel.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"", "", null, null, null, null},
+			},
+			new String[] {
+				"Course Name", "Course ID", "Instructor Name", "Credits", "Days", "Time"
+			}
+		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(82);
+		table.getColumnModel().getColumn(1).setPreferredWidth(83);
+		table.getColumnModel().getColumn(2).setPreferredWidth(103);
+		table.getColumnModel().getColumn(3).setPreferredWidth(91);
+		table.getColumnModel().getColumn(4).setPreferredWidth(86);
+		scrollPane.setViewportView(table);
+		
+		JLabel lblTitle = new JLabel("Courses Registration");
+		lblTitle.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitle.setBounds(247, 7, 297, 37);
+		getContentPane().add(lblTitle);
 		
 		JLabel lblCourseName = new JLabel("Course Name");
-		lblCourseName.setBounds(29, 58, 84, 14);
-		contentPane.add(lblCourseName);
+		lblCourseName.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblCourseName.setBounds(20, 76, 103, 14);
+		getContentPane().add(lblCourseName);
 		
 		txtCourseName = new JTextField();
-		txtCourseName.setBounds(137, 55, 117, 20);
-		contentPane.add(txtCourseName);
+		txtCourseName.setBounds(170, 76, 122, 17);
+		getContentPane().add(txtCourseName);
 		txtCourseName.setColumns(10);
 		
 		JLabel lblCourseId = new JLabel("Course ID");
-		lblCourseId.setBounds(29, 107, 84, 14);
-		contentPane.add(lblCourseId);
+		lblCourseId.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblCourseId.setBounds(20, 121, 75, 14);
+		getContentPane().add(lblCourseId);
 		
-		txtCourseID = new JTextField();
-		txtCourseID.setBounds(137, 104, 117, 20);
-		contentPane.add(txtCourseID);
-		txtCourseID.setColumns(10);
+		txtCourseId = new JTextField();
+		txtCourseId.setBounds(170, 121, 122, 17);
+		getContentPane().add(txtCourseId);
+		txtCourseId.setColumns(10);
 		
-		JLabel lblInstructor = new JLabel("Instructor Name");
-		lblInstructor.setBounds(29, 153, 98, 14);
-		contentPane.add(lblInstructor);
+		JLabel lblInstructorName = new JLabel("Instructor Name");
+		lblInstructorName.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblInstructorName.setBounds(20, 163, 128, 14);
+		getContentPane().add(lblInstructorName);
 		
 		txtInstructor = new JTextField();
-		txtInstructor.setBounds(137, 150, 117, 20);
-		contentPane.add(txtInstructor);
+		txtInstructor.setBounds(170, 163, 122, 17);
+		getContentPane().add(txtInstructor);
 		txtInstructor.setColumns(10);
 		
 		JLabel lblCredits = new JLabel("Credits");
-		lblCredits.setBounds(29, 198, 84, 14);
-		contentPane.add(lblCredits);
+		lblCredits.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblCredits.setBounds(20, 208, 75, 14);
+		getContentPane().add(lblCredits);
 		
-		txtCredit = new JTextField();
-		txtCredit.setBounds(137, 195, 117, 20);
-		contentPane.add(txtCredit);
-		txtCredit.setColumns(10);
+		JComboBox CreditBox = new JComboBox();
+		CreditBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5"}));
+		CreditBox.setBounds(170, 208, 103, 18);
+		getContentPane().add(CreditBox);
 		
 		JLabel lblDays = new JLabel("Days");
-		lblDays.setBounds(29, 242, 84, 14);
-		contentPane.add(lblDays);
+		lblDays.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblDays.setBounds(20, 260, 46, 14);
+		getContentPane().add(lblDays);
 		
-		txtDays = new JTextField();
-		txtDays.setBounds(137, 239, 117, 20);
-		contentPane.add(txtDays);
-		txtDays.setColumns(10);
+		JComboBox DaysBox = new JComboBox();
+		DaysBox.setModel(new DefaultComboBoxModel(new String[] {"M,T,W,TH,F", "M,T,TH", "M,W,F", "M,W", "M", "T,TH", "T,F", "T", "W,F", "W", "TH", "F", "SAT"}));
+		DaysBox.setBounds(170, 260, 103, 18);
+		getContentPane().add(DaysBox);
 		
 		JLabel lblTime = new JLabel("Time");
-		lblTime.setBounds(29, 281, 84, 14);
-		contentPane.add(lblTime);
+		lblTime.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblTime.setBounds(20, 316, 46, 14);
+		getContentPane().add(lblTime);
 		
-		txtTime = new JTextField();
-		txtTime.setBounds(137, 278, 117, 20);
-		contentPane.add(txtTime);
-		txtTime.setColumns(10);
+		JComboBox TimeBox = new JComboBox();
+		TimeBox.setModel(new DefaultComboBoxModel(new String[] {"8:00am - 8:50am", "9:05am - 10:45am", "9:55am - 10:45am", "9:55am - 11:55am", "10:55am - 12:45pm", "11:00am - 12:40pm", "1:10pm - 3:00pm", "2:15pm - 5:05pm", "4:00pm - 5:30pm", "6:00pm - 8:50pm"}));
+		TimeBox.setBounds(170, 315, 103, 18);
+		getContentPane().add(TimeBox);
 		
-		JButton btnAddCourses = new JButton("Add Courses");
-		btnAddCourses.setBounds(24, 358, 117, 23);
-		contentPane.add(btnAddCourses);
+		JButton btnAddCourse = new JButton("Add Course");
+		btnAddCourse.setBounds(20, 408, 114, 23);
+		getContentPane().add(btnAddCourse);
 		
-		JButton btnDeleteCourses = new JButton("Delete Courses");
-		btnDeleteCourses.setBounds(151, 358, 128, 23);
-		contentPane.add(btnDeleteCourses);
+		JButton btnDeleteCourse = new JButton("Delete Course");
+		btnDeleteCourse.setBounds(166, 408, 114, 23);
+		getContentPane().add(btnDeleteCourse);
 		
-		JButton btnPrintCourses = new JButton("Print Courses");
-		btnPrintCourses.setBounds(289, 358, 130, 23);
-		contentPane.add(btnPrintCourses);
+		JButton btnPrintCourse = new JButton("Print Course");
+		btnPrintCourse.setBounds(309, 408, 114, 23);
+		getContentPane().add(btnPrintCourse);
 		
-		JButton btnExit = new JButton("Exit");
-		btnExit.setBounds(429, 358, 128, 23);
-		contentPane.add(btnExit);
+		JButton btnSort = new JButton("Sort");
+		btnSort.setBounds(674, 408, 89, 23);
+		getContentPane().add(btnSort);
+		
+		JComboBox SortBox = new JComboBox();
+		SortBox.setModel(new DefaultComboBoxModel(new String[] {"Coure Name", "Course ID", "Instructor Name", "Credits", "Days", "Time"}));
+		SortBox.setBounds(575, 408, 89, 22);
+		getContentPane().add(SortBox);
 	}
 }
