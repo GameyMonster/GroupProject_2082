f2271ed65a4d38aabda7fc91ca9a9f0f0af5ddda
MarvinVa1995 <45765809+MarvinVa1995@users.noreply.github.com>
2019-12-04 14:26:45 -0600

    Update LoginGUI.java

--- Group Project/src/CollegeGUI/LoginGUI.java
+++ Group Project/src/CollegeGUI/LoginGUI.java
@@ -12,20 +12,25 @@ import javax.swing.JButton;
 import javax.swing.JPasswordField;
 import com.jgoodies.forms.factories.DefaultComponentFactory;
 
+import College.CreatingPasswordUsername;
+
 import javax.swing.JCheckBox;
 import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.Writer;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.awt.event.ActionEvent;
 
-public class LoginGUI {
+public class LoginGUI{
 
-	private JFrame frame;
+	JFrame frame;
 	private JTextField txtUser;
 	private JLabel lblPassword;
 	private JPasswordField passwordField;
@@ -34,6 +39,7 @@ public class LoginGUI {
 	 */
 	private final JLabel label = DefaultComponentFactory.getInstance().createTitle("New JGoodies title");
 	private JCheckBox chckbxShowPassword;
+	private ArrayList<CreatingPasswordUsername> userFiles = new ArrayList();
 
 	/**
 	 * Launch the application.
@@ -62,7 +68,7 @@ public class LoginGUI {
 		File check = new File("Student.txt");
 		if (check.exists()) {
 
-			// Checks if the file exists. will not add anything if the file does exist.
+			// Checks if the file exists. It will not add anything if the file does exist.
 		} else {
 			try {
 				File texting = new File("Student.txt");
@@ -98,46 +104,53 @@ public class LoginGUI {
 		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblPassword.setBounds(116, 114, 87, 14);
 		frame.getContentPane().add(lblPassword);
-		
-		//Sign in button
+
 		JButton btnSignIn = new JButton("Sign In");
+		//sign in button listener
 		btnSignIn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				String puname = txtUser.getText();
+				char[] password1 = passwordField.getPassword();
+				String ppaswd = new String(password1);
+				
+				
 				try {
-					File file = new File("Student.txt");
-					Scanner scan = new Scanner(file);
+					File f = new File("userFiles.txt");
+					FileInputStream fis = new FileInputStream(f);
+					ObjectInputStream ois = new ObjectInputStream(fis);
+					userFiles = (ArrayList<CreatingPasswordUsername>) ois.readObject();
+				}catch (FileNotFoundException e1) {
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (ClassNotFoundException e1) {
+					e1.printStackTrace();
+				}
+				
+				
+				
+				for(int x = 0; x < userFiles.size(); x++) {
 					
-					String line = null;
-					FileWriter filewrite = new FileWriter(file, true);
-
-					String usertxt = " ";
-					String passtxt = " ";
-					String puname = txtUser.getText();
-					String ppaswd = passwordField.getText();
-
-					while (scan.hasNext()) {
-						usertxt = scan.nextLine();
-						passtxt = scan.nextLine();
-
-					}
-
-					if (puname.equals(usertxt) && ppaswd.equals(passtxt)) {
-						JOptionPane.showMessageDialog(null, "Login Sucess!");
+					String stringOfPassword = userFiles.get(x).getPassword();
+					String stringOfUserName = userFiles.get(x).getUsername();
+					stringOfUserName = stringOfUserName.toLowerCase();
+					puname = puname.toLowerCase();
+			
+					if(puname.equals(stringOfUserName) && ppaswd.equals(stringOfPassword)) {
 						MainGUI menu = new MainGUI();
 						menu.setVisible(true);
-
-					} else if (puname.equals("") && ppaswd.equals("")) {
+						break;
+					}else if(puname.equals("") && ppaswd.equals("")) {
 						JOptionPane.showMessageDialog(null, "Please insert Username and Password");
-					} else {
-
+						break;
+					}else if(x == userFiles.size() - 1) {
 						JOptionPane.showMessageDialog(null, "Wrong Username / Password");
 						txtUser.setText("");
 						passwordField.setText("");
 						txtUser.requestFocus();
 					}
-				} catch (IOException d) {
-					d.printStackTrace();
 				}
+				
 			}
 		});
 		btnSignIn.setBounds(52, 190, 135, 23);
@@ -150,6 +163,7 @@ public class LoginGUI {
 				// Open the JFrame of the Account GUI
 				AccountGUI frame = new AccountGUI();
 				frame.setVisible(true);
+				
 			}
 		});
 		btnCreateAccount.setBounds(222, 190, 135, 23);
@@ -175,4 +189,6 @@ public class LoginGUI {
 		chckbxShowPassword.setBounds(201, 160, 97, 23);
 		frame.getContentPane().add(chckbxShowPassword);
 	}
+
+	
 }
